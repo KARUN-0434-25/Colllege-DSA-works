ad4196e7f8cf6a0b5e0f09e626b7c14d7a856882
KARUN-0434-25 <aruneshkr9@gmail.com>
2026-03-16 15:34:33 +0530

    practcing ...

--- SEM-II/Class-works/UNIT_01/main_03.cpp
+++ SEM-II/Class-works/UNIT_01/main_03.cpp
@@ -22,4 +22,25 @@ public:
         return -1; 
     }
 };
+//link of the leetcode -----https://leetcode.com/problems/search-insert-position/description/
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
 
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }else if (nums[mid] < target) {
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+};
