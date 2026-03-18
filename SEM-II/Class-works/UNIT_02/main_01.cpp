2e6bc75a50b79053efb5b56240f92c59d8e6c031
KARUN-0434-25 <aruneshkr9@gmail.com>
2026-03-18 10:19:49 +0530

    todaysa work

---
+++ SEM-II/Class-works/UNIT_02/main_01.cpp
@@ -0,0 +1,44 @@
+//COUNTING SORT
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " non-negative elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    int count[max + 1] = {0};
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i <= max; i++) {
+        while (count[i] > 0) {
+            arr[index] = i;
+            index++;
+            count[i]--;
+        }
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
